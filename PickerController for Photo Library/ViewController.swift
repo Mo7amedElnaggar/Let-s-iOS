5f8fcd6d9b7a6cd9dccd34160c633e230bd964e6
Mo7amedElnaggar <NaggarQ@gmail.com>
2017-03-30 04:03:43 +0200

    Add Deal With Photo Library

---
+++ PickerController for Photo Library/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Mego
+//
+//  Created by Mohamed El-Naggar on 3/21/17.
+//  Copyright © 2017 Mohamed El-Naggar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tapLabel: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add TapGestureRecognizer
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.selectImage))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func selectImage() {
+        print("Tap Image was Clicked")
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = false    // Dont allow user to Edit Photo
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("WELCOME")
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+    
+}
+
