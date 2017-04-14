066a5620761b8856b89ebae0aba38c66f270178a
Mo7amedElnaggar <NaggarQ@gmail.com>
2017-04-14 15:00:13 +0200

    Add initial View

---
+++ SmileFace_UIBezierPath/FaceVC.swift
@@ -0,0 +1,28 @@
+//
+//  FaceVC.swift
+//  CodeDoor
+//
+//  Created by Mohamed El Naggar on 4/9/17.
+//  Copyright © 2017 Mohamed El Naggar. All rights reserved.
+//
+
+import UIKit
+
+class FaceVC: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+    }
+    
+}
