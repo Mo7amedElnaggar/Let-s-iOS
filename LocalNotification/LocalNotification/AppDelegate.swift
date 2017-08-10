ff726697ecf18c6ab69936f06871771a8eaf3c51
Mo7amedElnaggar <NaggarQ@gmail.com>
2017-08-10 21:22:29 +0200

    LocalNotification

--- LocalNotification/LocalNotification/AppDelegate.swift
+++ LocalNotification/LocalNotification/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
         // Handle userNotification Request Authorization
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert , .sound]) { (success: Bool , error: Error?) in
