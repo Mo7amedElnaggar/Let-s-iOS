13dfaab58ab29a8dcfdb722e1c1dbf576f824c8a
Mo7amedElnaggar <NaggarQ@gmail.com>
2017-08-18 16:27:50 +0200

    SimpleLoginFrame

--- SimpleLoginFrame/SimpleLoginFrame/AppDelegate.swift
+++ SimpleLoginFrame/SimpleLoginFrame/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        if UIScreen.main.bounds.height == 667 {
+            
+        }
+        
+        
         return true
     }
 
